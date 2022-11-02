2601514b9702f658e37b56e447a61b2cec374686
Aimbas <facuu_arana@hotmail.com>
2022-11-02 01:02:47 -0300

    First Commit

--- client/main.lua
+++ client/main.lua
@@ -1,10 +1,54 @@
 ESX = nil
-local PlayerData = {}
 
 Citizen.CreateThread(function()
 	while ESX == nil do
 		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 		Citizen.Wait(0)
+	end
+end)
+
+Citizen.CreateThread(function()
+	for k,v in pairs(Aimcon.VehicleShops) do
+		local blip = AddBlipForCoord(v.x, v.y, v.z)
+		SetBlipSprite(blip,225)
+		SetBlipScale (blip, 0.8)
+		SetBlipAsShortRange(blip, true)
+	
+		BeginTextCommandSetBlipName('STRING')
+		AddTextComponentSubstringPlayerName("Conseccionario")
+		EndTextCommandSetBlipName(blip)
+	end
+end)
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(1500)
+		for k,v in pairs(Aimcon.VehicleShops) do
+			distance = #(GetEntityCoords(PlayerPedId()) - v)
+			if distance < 10.0 then 
+				actualShop = v 
+				actualDst = distance
+			else
+				actualShop = nil
+			end
+		end
+		
+	end
+end)
+
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		if actualShop then 
+			DrawMarker(21, actualShop, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 1.0, 1.0,0.8, 0, 255, 0, 100, false, true, 2, false, false,false, false)
+			if actualDst < 3.0 then 
+				ESX.ShowHelpNotification("Apreta ~INPUT_CONTEXT~ para ver la tienda")
+				if IsControlJustPressed(0, 38) then 
+					Aimcon.DrawMenu()
+				end
+			end
+		end
 	end
 end)
 
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,11 +5,13 @@ game 'gta5'
 version '1.1'
 
 server_scripts {
+	"@mysql-async/lib/MySQL.lua",
 	'config_sv.lua',
 	'server/main.lua'
 }
 
 client_scripts {
 	'config_cl.lua',
+	'client/functions.lua',
 	'client/main.lua'
 }
