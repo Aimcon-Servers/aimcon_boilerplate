fbd9f289bf4cd4a2870597478e1caef2a9b29ac6
Joaquin Giordano <joaquin_giordano@outlook.com>
2022-11-05 01:20:34 -0300

    Revert "First Commit"
    This reverts commit 2601514b9702f658e37b56e447a61b2cec374686.

--- config_cl.lua
+++ config_cl.lua
@@ -1,17 +1,1 @@
-Aimcon = {}
-
-Aimcon.VehicleShops = {
-    vector3(-29.75,-1105.03,26.42)
-}
-
-Aimcon.Vehicles = {
-    {label = "Blista", value ="blista"},
-    {label = "Stockade", value ="stockade"},
-}
-
-Aimcon.ZoneToViewVehicles = vector3(-47.01,-1098.41,26.42)
-Aimcon.ReturnZone = vector3(-29.75,-1105.03,26.42)
-
-Aimcon.TestDriveLoad = vector3(-29.76,-1105.04,19.9)
-Aimcon.TestDrive = vector3(-1325.7,-3151.01,13.95)
-Aimcon.TestDriveTime = 45
+Config = {}
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,13 +5,11 @@ game 'gta5'
 version '1.1'
 
 server_scripts {
-	"@mysql-async/lib/MySQL.lua",
 	'config_sv.lua',
 	'server/main.lua'
 }
 
 client_scripts {
 	'config_cl.lua',
-	'client/functions.lua',
 	'client/main.lua'
 }
